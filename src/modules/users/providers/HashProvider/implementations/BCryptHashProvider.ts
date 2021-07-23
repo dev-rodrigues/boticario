43b0e050be80efd37bad72f5b0af8ff2d84ea0a8
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2021-07-23 16:41:20 -0300

    logica para criptografia

--- src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -1,7 +1,7 @@
 import { compare, hash } from "bcryptjs";
 import IHashProvider from "../models/IHashProvider";
 
-class BCryptHashProvider implements IHashProvider{  
+class BCryptHashProvider implements IHashProvider {
   public async generateHash(payload: string): Promise<string> {
     return hash(payload, 8);
   }
